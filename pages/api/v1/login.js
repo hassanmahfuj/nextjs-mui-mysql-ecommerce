243121ca7dc2f441a17f644feef67379672512c9
Hassan Mahfuj <itsmahfuj@gmail.com>
2021-12-31 00:48:41 +0600

    login and register api updated

--- pages/api/v1/login.js
+++ pages/api/v1/login.js
@@ -1,15 +1,21 @@
+// imports
 import bcrypt from "bcryptjs";
 import db from "../../../lib/mariadb";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    // parsing data from request body
     let { email, password } = req.body;
-    email = typeof email === "string" && email.trim() ? email : false;
+
+    // validating user data
+    email =
+      typeof email === "string" && email.trim().length > 0 ? email : false;
     password =
       typeof password === "string" && password.trim().length > 0
         ? password
         : false;
 
+    // only procced if email and password is valid
     if (email && password) {
       try {
         const result = await db.find("users", "email", email);
@@ -17,23 +23,29 @@ export default async function handler(req, res) {
           const success = await bcrypt.compare(password, result[0].password);
           if (success) {
             res.status(200).json({ message: "Login Successful!" });
+
+            // password is not matched
           } else {
-            res
-              .status(403)
-              .json({ error: "There was a problem in your request!1" });
+            res.status(403).json({ error: "Invalid email or password!" });
           }
+
+          // email not found
         } else {
-          res
-            .status(404)
-            .json({ error: "There was a problem in your request!2" });
+          res.status(404).json({ error: "Email is not registered!" });
         }
+
+        // database error
       } catch (error) {
         res.status(500).json({ error: error.message });
       }
+
+      // email or password is invalid
     } else {
-      res.status(400).json({ error: "There was a problem in your request!3" });
+      res.status(400).json({ error: "There was a problem in your request!" });
     }
+
+    // not an accepted method
   } else {
-    res.status(405).json({ error: "There was a problem in your request!4" });
+    res.status(405).json({ error: "There was a problem in your request!" });
   }
 }
--- pages/api/v1/register.js
+++ pages/api/v1/register.js
@@ -1,37 +1,48 @@
+// imports
 import bcrypt from "bcryptjs";
 import db from "../../../lib/mariadb";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    let { name, email, password } = req.body;
-    name = typeof name === "string" && name.trim().length > 0 ? name : false;
-    email = typeof email === "string" && email.trim() ? email : false;
+    // parsing data from request body
+    let { email, password } = req.body;
+
+    // validating user data
+    email =
+      typeof email === "string" && email.trim().length > 0 ? email : false;
     password =
       typeof password === "string" && password.trim().length > 0
         ? password
         : false;
 
-    if (name && email && password) {
+    // only procced if email and password is valid
+    if (email && password) {
       try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await db.push("users", {
-          name,
-          email,
-          address: "hpur",
-          password: hashedPassword,
-        });
-        res.status(200).json({ message: "Signup Successful!" });
+        // checking email is in database or not
+        const result = await db.find("users", "email", email);
+        if (result.length > 0) {
+          res.status(501).json({ error: "Email is already registered!" });
+        } else {
+          const hashedPassword = await bcrypt.hash(password, 10);
+          await db.push("users", {
+            email,
+            password: hashedPassword,
+          });
+          res.status(200).json({ message: "Signup Successful!" });
+        }
+        // database error
       } catch (error) {
-        console.log(error.message);
-        res.status(500).json({
-          error: error.message,
-        });
+        res.status(500).json({ error: error.message });
       }
+
+      // email or password is invalid
     } else {
       res.status(400).json({
         error: "There was a problem in your request!",
       });
     }
+
+    // not an accepted method
   } else {
     res.status(405).json({ error: "There was a problem in your request!" });
   }
